02575478651c83eca1fe7437a50f399bb7029edd
imed-hamdi <imedh00u@gmail.com>
2021-05-17 00:26:18 +0200

    responsive et panier

--- apii.js
+++ apii.js
@@ -1,26 +1,73 @@
 
 
+
+function display() {
+
+    let peluche = document.getElementById("main");
+    peluche.innerHTML += `
+    
+    <a href="produit.html"   onclick="displayproduit()">
+       <li id="carte">
+        <div class="  card-body " >
+        <img class=”card-img-top" src=${post.imageUrl}>
+        <hr>
+        <h4 class="card-title">${post.name}</h4>
+       
+        <div class="price-btn">
+        <p class="card-text prix">${post.price / 100} €</p>
+        <button type="button" class="btn btn-dark">Acheter</button>
+        </div>
+        <hr>
+        <p class="card-text">${post.description}</p>
+        
+
+       </div>
+       </li>
+       </a>
+       
+`
+
+}
+
+
 fetch('  http://localhost:3000/api/teddies')
     .then(response => response.json())
     .then(function (posts) {
         for (post of posts) {
-
-            document.getElementById("main").innerHTML += `
-            <ul class="list-product">
-            <a href="#">
-             <li>
-            <div class="  card-body " >
-                <img class=”card-img-top crtaa" src=${post.imageUrl}>
-                <hr>
-                <h4 class="card-title">${post.name}</h4>
-                <p class="card-text">${post.price}$</p>
-                <button type="button" class="btn btn-dark">acheter</button>
-               
-               </div>
-               </li>
-               </a>
-               </ul>
-       `
+            display();
 
         }
+
+
     });
+
+
+ 
+    fetch('  http://localhost:3000/api/teddies')
+    .then(response => response.json())
+    .then(function(posts) {
+        for(let i=0;i<posts.length;i++){
+            if (post.name = `${posts[i].name}`){
+                displayproduit()
+            }
+    }  
+});
+
+        
+function displayproduit() {
+let teddi = document.getElementById("main-produit");
+teddi.innerHTML += `   
+<img class=”card-img-top" src=${post.imageUrl}>
+<hr>
+<h4 class="card-title">${post.name}</h4>
+<div class="price-btn">
+<p class="card-text">${post.price / 100} €</p>
+<button type="button" class="btn btn-dark">Acheter</button>
+</div>
+<hr>
+<p class="card-text">${post.description}</p>
+<p class="card-text">${post._id}</p>
+</div>
+`
+
+}
