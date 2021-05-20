8face3e2935ffaefc8239ae311f846f6a55ad6c1
imed-hamdi <imedh00u@gmail.com>
2021-05-20 13:43:27 +0200

    formulaire-option

--- produit.js
+++ produit.js
@@ -6,24 +6,40 @@ async function func() {
     const id = url.get("id");
     const product = await getProductbyid(id);
     displayproduit(product)
+
 }
 
 
 function displayproduit(product) {
 
-    const produit = document.getElementById("main-produit");
-    produit.innerHTML = `<div class="  card-body card-product" >
-    <img class=”card-img-top" src=${product.imageUrl}>
-    <div class="card-description>
-       <h4 class="card-title">${product.name}</h4>
-        <div class="price-btn">
-          <p class="card-text prix">${product.price / 100} €</p>
-          <button type="button" class="btn btn-dark">Ajouter</button>
-        </div>
-        <p class="card-text card-text-descreption">${product.description}</p>
-    
-    </div>
-    </div>`
+const produit = document.getElementById("main-produit");
+let colorsNumb = product.colors;
+let newdiv =[];
+ for(i=0;i<colorsNumb.length;i++){
+   newdiv +=`
+   <option value="${i+1}">${colorsNumb[i]}</option>;`
+  }
+ produit.innerHTML = `
+ <div class="  card-body card-product" >
+ <img class=”card-img-top" src=${product.imageUrl}>
+
+<form>
+<label for="options-couleurs">Choisir la couleur</label>
+<select name="colors" id="options-colors">
+${newdiv}
+</select> 
+</form>
+
+<hr>
+<div id="name-price">
+<h4 class="card-title">${product.name}</h4>
+<p class="card-text prix ">${product.price / 100} €</p>
+<button type="button" class="btn btn-dark">Acheter</button>
+</div>  
+<hr>
+<p class="card-text">${product.description}</p>
+</div>
+    `
 }
 function getProductbyid(id) {
     return fetch(`http://localhost:3000/api/teddies/${id}`)
